31c7fb5dd2026edf03f554fe648119dc12ad9ad9
Guillaume Sentenac <guillaumesentenac@gmail.com>
2014-05-27 22:50:43 +0000

    latest

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var express = require('express')
-var app = express();
+var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger()); 
+
+app.get('/', function(request, response) {
+var html = fs.readFileSync(htmlfile).toString();
+response.send(html);
+});
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
